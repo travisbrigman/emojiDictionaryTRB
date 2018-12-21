47d1be475879a6287e511c2c082226bb4659ffd1
Travis Brigman <travis1000@gmail.com>
2018-12-21 09:29:05 -0600

    Initial Commit

---
+++ emojiDictionaryTRB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  emojiDictionaryTRB
+//
+//  Created by Travis Brigman on 12/21/18.
+//  Copyright © 2018 Travis Brigman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
